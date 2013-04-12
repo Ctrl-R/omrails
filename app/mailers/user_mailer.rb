cedc5331dea8414700f5049c33c9e6682c63cf89
Ian Randolph <CtrlR7+git@gmail.com>
2013-04-12 17:04:46 -0400

    Fix reply email issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default :from => "ian@ctrl-r.com"
   
   def request_pin(user, pin)
     @user = user
     @pin = pin
-    mail(:to => "#{@pin.user.name} <#{@pin.user.email}>", :reply_to => @user.email, :subject => "#{@user.name} has requested #{@pin.description}")
+    mail(:to => "#{@pin.user.name} <#{@pin.user.email}>", :from => @user.email, :reply_to => @user.email, :subject => "#{@user.name} has requested #{@pin.description}")
   end
   
 end

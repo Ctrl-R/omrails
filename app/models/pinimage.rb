352cd611045c933f3a87f36ec2e617761c12c0dc
Ian Randolph <CtrlR7+git@gmail.com>
2013-11-23 22:21:16 -0500

    Fix pinimage model

--- app/models/pinimage.rb
+++ app/models/pinimage.rb
@@ -1,9 +1,10 @@
 class Pinimage < ActiveRecord::Base
   attr_accessible :caption, :image, :pin
   
+  has_attached_file :image, styles: { thumb: "150x150>", :convert_options => "-auto-orient"}
+  
   validates_attachment :image, content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
                                size: { less_than: 5.megabytes }
   
   belongs_to :pin
-  has_attached_file :image, styles: { thumb: "150x150>"}
 end

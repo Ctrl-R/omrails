f15e8509ae1f6bb0ef23a3364ce465dd09cf9ca9
Ian Randolph <CtrlR7+github@gmail.com>
2014-02-13 18:06:46 -0500

    Delete 20131115021814_change_column_names_to_all_lowercase.rb

--- db/migrate/20131115021814_change_column_names_to_all_lowercase.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnNamesToAllLowercase < ActiveRecord::Migration
-  def change
-    rename_column :loans, :loanedOn, :loanedon
-    rename_column :loans, :loanedTo, :loanedto
-    rename_column :loans, :returnedOn, :returnedon
-    rename_column :pins, :forSale, :forsale
-    rename_column :pins, :forLoan, :forloan
-  end
-end

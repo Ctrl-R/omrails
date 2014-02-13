d288b5b56debb572d88f3cb88bbac3164473e8f9
Ian Randolph <CtrlR7+github@gmail.com>
2014-02-13 18:07:04 -0500

    Delete 20131124190042_add_user_column_to_pinimages.rb

--- db/migrate/20131124190042_add_user_column_to_pinimages.rb
+++
@@ -1,7 +0,0 @@
-class AddUserColumnToPinimages < ActiveRecord::Migration
-  def change
-    add_column :pinimages, :user_id, :integer
-    add_index :pinimages, :pin_id
-    add_index :pinimages, :user_id
-  end
-end
